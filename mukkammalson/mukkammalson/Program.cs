e59d6d3045741c34b0ca709a26869e0988887296
Toleyorbek <128204542+Toleyorbek@users.noreply.github.com>
2023-04-05 09:53:24 +0500

    mukammal son

---
+++ mukkammalson/mukkammalson/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace mukkammalson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+
+                for (int j = 1; j <= b / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        Console.WriteLine(i / j);
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
